8b50417d2c5062a6bf9b7826aec13451e3bab09d
Deeps-L <deepikalakhera9@gmail.com>
2024-05-10 11:44:05 +0530

    app js changes ans server file also

--- server.js
+++ server.js
@@ -1,143 +1,56 @@
-// const express = require("express");
-// const http = require("http");
-// const socketIO = require("socket.io");
-// const cors = require("cors");
-// const app = express();
-// const server = http.createServer(app);
-// const io = socketIO(server, {
-//     cors: {
-//         origin: "*",
-//         methods: ["GET", "POST"],
-//     },
-// });
-// app.use(cors());
-// const PORT = process.env.PORT || 3001;
-// let connectedUsers = {};
-// let adminSocketId = null;
-// io.on("connection", (socket) => {
-//     if (!adminSocketId) {
-//         adminSocketId = socket.id;
-//         socket.emit("admin");
-//         socket.join("adminRoom");
-//     } else {
-//         socket.emit("user");
-//         socket.join("userRoom");
-//     }
-
-//     socket.on("admin", () => {
-//         console.log(`Admin is connected`);
-//         io.to(socket.id).emit("admin");
-//     });
-
-//     socket.on("user", () => {
-//         console.log(`User is connected`);
-//         io.to(socket.id).emit("user");
-//     });
-
-//     socket.on("ballMoved", (data) => {
-//         io.to("userRoom").emit("ballMoved", data);
-//     });
-//     socket.on("adminButtonClicked", (data) => {
-//         io.to("userRoom").emit("adminButtonClicked", data);
-//     });
-
-//     connectedUsers[socket.id] = userRole;
-
-//     // Emit the role to the client
-//     socket.emit("roleAssigned", userRole)
-
-
-//     socket.on("disconnect", () => {
-//         if (socket.id === adminSocketId) {
-//             console.log("Admin is disconnected");
-//             adminSocketId = null;
-//         }
-//         socket.leave("adminRoom");
-//         socket.leave("userRoom");
-//     });
-// });
-
-// server.listen(PORT, () => {
-//     console.log(`Server is running on ${PORT}`);
-// });
-
-
-
-// Import necessary modules
 const express = require("express");
 const http = require("http");
 const socketIO = require("socket.io");
 const cors = require("cors");
-const path = require("path");
-// Create Express app
 const app = express();
-
-// Create HTTP server
 const server = http.createServer(app);
-
-// Create Socket.IO server
 const io = socketIO(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"],
-    },
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
-
-// Use CORS middleware
 app.use(cors());
+const PORT = process.env.PORT || 3001;
 
-
-
-// Define route handler for the root route
-
-
-let connectedUsers = {};
 let adminSocketId = null;
 io.on("connection", (socket) => {
-    if (!adminSocketId) {
-        adminSocketId = socket.id;
-        socket.emit("admin");
-        socket.join("adminRoom");
-    } else {
-        socket.emit("user");
-        socket.join("userRoom");
+  if (!adminSocketId) {
+    adminSocketId = socket.id;
+    socket.emit("admin");
+    socket.join("adminRoom");
+  } else {
+    socket.emit("user");
+    socket.join("userRoom");
+  }
+
+  socket.on("admin", () => {
+    console.log(`Admin is connected`);
+    io.to(socket.id).emit("admin");
+  });
+
+  socket.on("user", () => {
+    console.log(`User is connected`);
+    io.to(socket.id).emit("user");
+  });
+
+  socket.on("ballMoved", (data) => {
+    io.to("userRoom").emit("ballMoved", data);
+  });
+  socket.on("adminButtonClicked", (data) => {
+    io.to("userRoom").emit("adminButtonClicked", data);
+  });
+
+  socket.on("disconnect", () => {
+    if (socket.id === adminSocketId) {
+      console.log("Admin is disconnected");
+      adminSocketId = null;
     }
-
-    socket.on("admin", () => {
-        console.log(`Admin is connected`);
-        io.to(socket.id).emit("admin");
-    });
-
-    socket.on("user", () => {
-        console.log(`User is connected`);
-        io.to(socket.id).emit("user");
-    });
-
-    socket.on("ballMoved", (data) => {
-        io.to("userRoom").emit("ballMoved", data);
-    });
-    socket.on("adminButtonClicked", (data) => {
-        io.to("userRoom").emit("adminButtonClicked", data);
-    });
-
-    // connectedUsers[socket.id] = userRole;
-
-    // // Emit the role to the client
-    // socket.emit("roleAssigned", userRole)
-
-
-    socket.on("disconnect", () => {
-        if (socket.id === adminSocketId) {
-            console.log("Admin is disconnected");
-            adminSocketId = null;
-        }
-        socket.leave("adminRoom");
-        socket.leave("userRoom");
-    });
+    socket.leave("adminRoom");
+    socket.leave("userRoom");
+  });
 });
 
-// Start the server
-const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`);
+  console.log(`Server is running on ${PORT}`);
 });
--- src/App.js
+++ src/App.js
@@ -5,34 +5,27 @@ import io from "socket.io-client";
 
 const App = () => {
   const phaserRef = useRef(null);
-  let ball;
+  const ball = useRef(null);
+  const game = useRef(null);
+  const socket = useRef(null);
 
-  // const [role, setRole] = useState(""); 
-  const socket = io("http://localhost:3001");
-  function handleClick(targetX, targetY, speed) {
-    // Calculate the direction towards the clicked button
-
-    
-    const velocityX = targetX - ball.x;
-    const velocityY = targetY - ball.y;
-     ball.speed = 500;
-    // Set the velocities
-    ball.setVelocity(velocityX * speed, velocityY * speed);
-
-    socket.emit("adminButtonClicked", { targetX, targetY });
-  }
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [clickedButtons, setClickedButtons] = useState([]);
 
   useEffect(() => {
+    socket.current = io("http://localhost:3001");
 
-   
-    socket.on("connect", () => {
+    socket.current.on("connect", () => {
       console.log("Connected to server");
     });
-
-    // socket.on("roleAssigned", (userRole) => {
-    //   setRole(userRole);
-    // });
-
+    socket.current.on("admin", () => {
+      console.log("Received admin button number");
+      setIsAdmin(true);
+    });
+    socket.current.on("user", () => {
+      console.log("Received user number");
+      setIsAdmin(false);
+    });
 
     const config = {
       type: Phaser.AUTO,
@@ -53,58 +46,110 @@ const App = () => {
       },
     };
 
-    const game = new Phaser.Game(config);
+    game.current = new Phaser.Game(config);
 
     function preload() {
       this.load.image("Tennis", "./assets/tennisBall.jpg");
     }
 
     function create() {
-      ball = this.physics.add.image(400, 300, "Tennis");
-      ball.setCollideWorldBounds(true);
-      ball.setBounce(1);
-      ball.setVelocity(250, 250);
+      socket.current.emit(isAdmin ? "admin" : "user");
+      ball.current = this.physics.add.sprite(400, 300, "Tennis");
+      ball.current.setCollideWorldBounds(true);
+      ball.current.setBounce(1);
+      ball.current.setVelocity(250, 250);
+      this.physics.world.setBoundsCollision(true, true, true, true);
+
+      socket.current.on("ballMoved", (data) => {
+        ball.current.setVelocity(data.velocityX, data.velocityY);
+      });
     }
 
     function update() {
-      // Update logic here
+      // Move logic here if needed
     }
 
     return () => {
-      game.destroy(true);
-      socket.disconnect();
+      game.current.destroy(true);
+      socket.current.disconnect();
     };
   }, []);
 
+  useEffect(() => {
+  socket.current.on("userButtonClicked", ({ x, y, buttonName }) => {
+    if (isAdmin) {
+      // Emit the button click data to all users, including the admin
+      socket.current.emit("adminButtonClicked", buttonName);
+    }
+    setClickedButtons([...clickedButtons, buttonName]);
+  });
+}, [isAdmin, clickedButtons]);
+
+
+  const handleClick = (targetX, targetY, speed, buttonName) => {
+    const velocityX = targetX - ball.current.x;
+    const velocityY = targetY - ball.current.y;
+  
+    ball.current.setVelocity(velocityX * speed, velocityY * speed);
+  
+    if (!isAdmin) {
+      // Emit the button click data to the server
+      socket.current.emit("userButtonClicked", { x: targetX, y: targetY, buttonName });
+    }
+  
+    setClickedButtons([...clickedButtons, buttonName]);
+  };
+  
+
   return (
     <div className="container">
-      <h1>Hello User!</h1>
+      <div id="right-panel">
+        <h2 className="text-lg font-extrabold">
+          {isAdmin ? (
+            <div style={{ textAlign: 'center', marginBottom: '10px' }}>
+              <p className="text-center justify-center items-center">ADMIN</p>
+              
+            </div>
+          ) : (
+            <div className="flex" style={{ textAlign: 'center', marginBottom: '10px' }}>
+              <p className="text-lg ml-96 font-extrabold">USER</p>
+              {clickedButtons.length > 0 && (
+                <h6><i>
+                  <p className="ml-96 text-lg font-extrabold">
+                    Buttons clicked by admin: {clickedButtons.join(", ")}
+                  </p>
+                </i></h6>
+              )}
+            </div>
+          )}
+        </h2>
+      </div>
       <div className="canvas-container" ref={phaserRef}></div>
       <div className="btnGroup">
         <button
           id="btn1"
-          onClick={() => handleClick(200, 0, 1)}
+          onClick={() => handleClick(200, 0, 1, "Button 1")}
           className="button"
         >
           Button 1
         </button>
         <button
           id="btn2"
-          onClick={() => handleClick(500, 0, 1.5)}
+          onClick={() => handleClick(500, 0, 1.5, "Button 2")}
           className="button"
         >
           Button 2
         </button>
         <button
           id="btn3"
-          onClick={() => handleClick(800, 160, 1.5)}
+          onClick={() => handleClick(800, 160, 1.5, "Button 3")}
           className="button"
         >
           Button 3
         </button>
         <button
           id="btn4"
-          onClick={() => handleClick(800, 450, 1)}
+          onClick={() => handleClick(800, 450, 1, "Button 4")}
           className="button"
         >
           Button 4
@@ -112,14 +157,14 @@ const App = () => {
 
         <button
           id="btn5"
-          onClick={() => handleClick(200, 600, 1)}
+          onClick={() => handleClick(200, 600, 1, "Button 5")}
           className="button"
         >
           Button 5
         </button>
         <button
           id="btn6"
-          onClick={() => handleClick(500, 600, 1)}
+          onClick={() => handleClick(500, 600, 1, "Button 6")}
           className="button"
         >
           Button 6
@@ -127,14 +172,14 @@ const App = () => {
 
         <button
           id="btn7"
-          onClick={() => handleClick(0, 200, 1)}
+          onClick={() => handleClick(0, 200, 1, "Button 7")}
           className="button"
         >
           Button 7
         </button>
         <button
           id="btn8"
-          onClick={() => handleClick(0, 400, 1)}
+          onClick={() => handleClick(0, 400, 1, "Button 8")}
           className="button"
         >
           Button 8
